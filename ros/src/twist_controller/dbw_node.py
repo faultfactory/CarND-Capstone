01e8a4b7774f5f380640e930255a56d12c936d6f
faultfactory <32714474+faultfactory@users.noreply.github.com>
2018-09-01 15:44:59 -0400

    angular vel input

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -99,7 +99,7 @@ class DBWNode(object):
 
     def twist_cb(self,msg):
         self.linear_vel = msg.twist.linear.x
-        self.angular_vel = msg.twist.angular.x
+        self.angular_vel = msg.twist.angular.z
     
     def velocity_cb(self,msg):
         self.current_vel = msg.twist.linear.x
