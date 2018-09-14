7f66d0a3c63c5530b8b30db972a62f318909fe86
Jesse <32714474+faultfactory@users.noreply.github.com>
2018-09-14 01:01:12 +0000

    successful test of trained networks

--- grabModelGit.sh
+++ grabModelGit.sh
@@ -4,17 +4,3 @@ cd /home/workspace
 git clone https://www.github.com/tensorflow/models.git
 cd models
 git checkout f7e99c0
-rm -rf .git
-rm -rf tutorials
-
-cd /home/workspace
-mkdir modelzoo
-cd modelzoo
-wget http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_coco_11_06_2017.tar.gz
-tar -xzf ssd_mobilenet_v1_coco_11_06_2017.tar.gz
-rm -f /home/workspace/modelzoo/ssd_mobilenet_v1_coco_11_06_2017.tar.gz
-
-
-wget http://download.tensorflow.org/models/object_detection/ssd_inception_v2_coco_11_06_2017.tar.gz
-tar -xzf ssd_inception_v2_coco_11_06_2017.tar.gz
-rm -f /home/workspace/modelzoo/ssd_inception_v2_coco_11_06_2017.tar.gz
--- workspace_setup_for_training.sh
+++ workspace_setup_for_training.sh
@@ -14,3 +14,9 @@ python setup.py install
 protoc object_detection/protos/*.proto --python_out=.
 #cd /home/workspace/models
 export PYTHONPATH=$PYTHONPATH:/home/workspace/models:/home/workspace/models/research:/home/workspace/models/research/slim
+
+cd /home/workspace/models/research/object_detection/
+wget https://s3.amazonaws.com/udacity-transfer/frozen_inference_graph_real.pb
+wget https://s3.amazonaws.com/udacity-transfer/frozen_inference_graph_sim.pb
+
+apt-get install chromium-browser -y
