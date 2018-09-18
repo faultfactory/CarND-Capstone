db469aaae1278ab0f96c6c811eaea87868ed9512
Jesse <32714474+faultfactory@users.noreply.github.com>
2018-09-18 14:08:22 -0400

    adding preeti's code for tl classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,19 +1,58 @@
 from styx_msgs.msg import TrafficLight
 
+import tensorflow as tf 
+import numpy as np 
+import cv2
+
 class TLClassifier(object):
-    def __init__(self):
+    def __init__(self, is_site):
         #TODO load classifier
-        pass
+
+        # path to graph
+        if is_site:
+        	PATH_FROZEN_GRAPH = r'models/frozen_inference_graph_real.pb'
+        else:
+            PATH_FROZEN_GRAPH = r'models/frozen_inference_graph_sim.pb'
+        
+        # gpu config
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        
+    def load_graph(graph_path):
+        graph = tf.Graph()
+        # load the protobuf file 
+        with tf.gfile.GFile(graph_path, "rb") as f:
+        	graph_def = tf.GraphDef()
+        	graph_def.ParseFromString(f.read())
+        # import the graph 
+        with self.graph.as_default():
+        	tf.import_graph_def(graph_def, name='')
+        return graph
+
+    def pre_process(self, image):
+        img = np.array(image, dtype="uint8")
+        return img
+
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
-
         Args:
             image (cv::Mat): image containing the traffic light
-
         Returns:
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
         """
         #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        image_in = pre_process(image)
+        graph_in = load_graph(graph_path)
+
+        
+        with graph_in.as_default():
+        	with tf.Session(graph=graph_in, config=config) as sess:
+        	"""
+            color_id = graph_in.get_tensor_by_name(???)
+            (???) = sess.run([???],
+            	          feed_dict = {color_tensor: image_in})
+            TrafficLight.state = ???
+            return TrafficLight.state
+            """
+return TrafficLight.UNKNOWN
